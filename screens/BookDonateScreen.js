11fea7014051503d3bb6d72e1ba48cc3c1c962da
Hartej Nayar <71260380+H1103@users.noreply.github.com>
2021-05-06 12:39:47 +0530

    Add files via upload

---
+++ screens/BookDonateScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Text, View, StyleSheet, TextInput, Alert, } from 'react-native';
+
+export default class BookDonateScreen extends Component {
+    render(){
+        return(
+            <View>
+                <Text>Book Donate Screen</Text>
+            </View>
+        );   
+     }
+}
---
+++ screens/BookRequestScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {Text, View, StyleSheet, TextInput, Alert} from 'react-native';
+
+export default class BookRequestScreen extends Component {
+    render(){
+        return(
+            <View>
+                <Text> Book Request Screen </Text>
+            </View>
+        );   
+     }
+}
